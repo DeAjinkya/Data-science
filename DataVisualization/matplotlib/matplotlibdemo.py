2372a689be3ddcc92c3d1b626d84ed597b0a92af
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-05-29 19:57:34 +0530

    Matplotlib

---
+++ DataVisualization/matplotlib/matplotlibdemo.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3],[4,5,6])
+plt.show()
+
+#Interacting with the Plot
+#When a plot appears, you can:
+
+#Zoom In/Out
+#Pan around
+#Use arrows to navigate history
+#Reset to home
+#Save as PNG using the disk icon
+#These features are automatically included in the plot window!
